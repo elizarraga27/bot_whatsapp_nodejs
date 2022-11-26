481240399ff2cd046afbc75d6552231ecb935f4e
elizarraga <eduardo.alejandro.lizarraga@gmail.com>
2022-11-26 10:34:45 -0300

    cardapio

---
+++ src/cardapio.js
@@ -0,0 +1,16 @@
+const cardapio = {
+    1:{
+        descricao: "X-Burguer",
+        preco: 9.90
+    },
+    2:{
+        descricao: "Passaporte",
+        preco: 10.00
+    },
+    3:{
+        descricao: "Pizza",
+        preco: 12.99
+    }
+};
+
+exports.menu = cardapio;
